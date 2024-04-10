64ccce518c4169b7d9f886ec370ceee6aaca98e9
jainl28patel <jainilpatel2003@gmail.com>
2024-04-11 02:17:14 +0530

    adds manager flow

--- src/routes/manager.mjs
+++ src/routes/manager.mjs
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-import { usersModel, storesModel, salesModel, medicinesModel } from "../db/models.mjs";
+import { usersModel, storesModel, salesModel, medicinesModel, inventoryModel } from "../db/models.mjs";
 
 const isManager = async (req, res, next) => {
     // get jwt from header
@@ -24,5 +24,118 @@ const isManager = async (req, res, next) => {
         res.sendStatus(403);
     }
 }
+
+router.get("/medicines", isManager, async (req, res)=>{
+    if(!req.user.id) {
+        return res.sendStatus(403)
+    }
+
+    try {
+        let inventory = await inventoryModel.findOne({manager_id: req.user._id});
+        let response = [];
+        inventory.medicines.forEach(med => async () => {
+            let medicine = await medicinesModel.findOne({_id: med.medicine});
+            response.push({
+                id: medicine._id,
+                name: medicine.name,
+                sales: med.qty_sold,
+                ordered: med.qty_ordered,
+                available: med.qty_available,
+            });
+        });
+        res.send(response);
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+router.get("/inventory", isManager, async (req, res)=>{
+    if(!req.user.id) {
+        return res.sendStatus(403)
+    }
+
+    try {
+        let inventory = await inventoryModel.findOne({manager_id: req.user._id});
+        let response = [];
+        inventory.medicines.forEach(med => async () => {
+            let medicine = await medicinesModel.findOne({_id: med.medicine});
+            response.push({
+                id: medicine._id,
+                name: medicine.name,
+                sales: med.qty_sold,
+                ordered: med.qty_ordered,
+                available: med.qty_available,
+                expiry_date: medicine.expiry_date,
+                quantity: med.qty_available,
+                batch_id: med.batch_id,
+            });
+        });
+        res.send(response);
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+router.post("/update", isManager, async (req, res)=>{
+    if(!req.user.id) {
+        return res.sendStatus(403)
+    }
+    let {medicine_name, quantity, batch_id, expiry_date, price} = req.body;
+    try {
+        let medicine = await medicinesModel.findOne({name: medicine_name});
+        let inventory = await inventoryModel.findOne({manager_id: req.user._id});
+        let med = inventory.medicines.find(med => med.medicine == medicine._id);
+        if(!medicine) {
+            // create new medicine
+            let newMed = new medicinesModel({
+                name: medicine_name,
+                expiry_date: expiry_date,
+                price: price,
+                qty_available: quantity,
+                qty_sold: 0,
+                qty_ordered: 0,
+            });
+            await newMed.save();
+            inventory.medicines.push({
+                medicine: newMed._id,
+                qty_available: quantity,
+                qty_ordered: 0,
+                qty_sold: 0,
+                batch_id: batch_id,
+            });
+            inventory.save();
+            res.sendStatus(200);
+        } else if (!med) {
+            // add medicine to inventory
+            inventory.medicines.push({
+                medicine: medicine._id,
+                qty_available: quantity,
+                qty_ordered: 0,
+                qty_sold: 0,
+                batch_id: batch_id,
+            });
+            inventory.save();
+
+            // increment medicine quantity
+            medicine.qty_available += quantity;
+            medicine.save();
+            res.sendStatus(200);
+        } else {
+            // update medicine quantity
+            med.qty_available += quantity;
+            med.save();
+
+            // increment medicine quantity
+            medicine.qty_available += quantity;
+            medicine.save();
+            res.sendStatus(200);
+        }
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
 
 export default router;
