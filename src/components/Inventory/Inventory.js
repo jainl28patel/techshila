2b5bf24c94489f979cf4b6a067ba510526f41086
vinayIITian <98804719+vinayIITian@users.noreply.github.com>
2024-04-11 17:03:23 +0530

    updated inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -8,161 +8,172 @@ import dots from "../../assets/DotsThreeVertical.svg"
 const Inventory = () => {
     const nodes = [
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: red,
-                name: "Low",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "1",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: red,
+            name: "Low",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: grey,
-                name: "Expired",
-                name2: "Very High",
-                team: "16-18 Days",
-            },
-            contact: "7605216585",
+          id: "2",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: grey,
+            name: "Expired",
+            name2: "Very High",
+            team: "16-18 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: yellow,
-                name: "Medium",
-                name2: "Medium",
-                team: "8-10 Days",
-            },
-            contact: "7605216585",
+          id: "3",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: yellow,
+            name: "Medium",
+            name2: "Medium",
+            team: "8-10 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "4",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "5",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "6",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "7",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "8",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "9",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "10",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         {
-            store_id: "6efbdA234",
-            batch_id: "6efbdA234",
-            store_name: "Ecosprin 75mg Strip Of 14 Tablets",
-            sales: "14x50",
-            oderered: "March 2024",
-            store_manager: {
-                img: green,
-                name: "High",
-                name2: "Low",
-                team: "4-5 Days",
-            },
-            contact: "7605216585",
+          id: "11",
+          store_id: "6efbdA234",
+          batch_id: "6efbdA234",
+          store_name: "Ecosprin 75mg Strip Of 14 Tablets",
+          sales: "14x50",
+          oderered: "March 2024",
+          store_manager: {
+            img: green,
+            name: "High",
+            name2: "Low",
+            team: "4-5 Days",
+          },
+          contact: "7605216585",
         },
         // Add more nodes as needed
-    ];
+      ];
 
     const [searchTerm, setSearchTerm] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
@@ -171,6 +182,29 @@ const Inventory = () => {
     const [individualCheckboxes, setIndividualCheckboxes] = useState(
         Array(nodes.length).fill(false)
     );
+
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+    const [modal, setModal] = useState(false);
+    const [Drop, setDrop] = useState({ state: true, id: "" });
+
+    const handleModalChange = () => {
+        setModal(!modal);
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        const files = [...e.dataTransfer.files];
+        setUploadedFiles(files);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleFileUpload = (e) => {
+        const files = [...e.target.files];
+        setUploadedFiles(files);
+    };
 
 
     const handleMainCheckboxChange = (event) => {
@@ -214,12 +248,13 @@ const Inventory = () => {
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
-                <span
+                <button
                     className=" text-white store-create "
-                    style={{ padding: "5px 40px", background: "#303F9F" }}
+                    style={{ padding: "5px 40px", background: "#303F9F", cursor: "pointer" }}
+                    onClick={handleModalChange}
                 >
                     Update Inventory
-                </span>
+                </button>
             </div>
             <div className="table">
                 <div class="relative overflow-x-auto">
@@ -256,6 +291,7 @@ const Inventory = () => {
                         </thead>
                         {/* <hr style={{ width: "100%" }} /> */}
                         <tbody>
+
                             {currentItems?.map((el, index) => {
                                 return (
                                     <tr class="bg-white border-b">
@@ -294,7 +330,39 @@ const Inventory = () => {
                                             </div>
                                         </td>
                                         <td class="px-6 py-2 text-gray-800">{el.oderered}</td>
-                                        <td class="px-6 py-2 text-gray-800 cursor-pointer"><img src={dots} alt="dots" /></td>
+                                        <td class="px-6 py-2 text-gray-800 cursor-pointer"><img src={dots} onClick={() => {
+                                            setDrop({ ...Drop, ["id"]: el.id, ["state"]: true });
+                                        }} alt="dots" />
+                                            {Drop.id === el.id && Drop.state && (
+                                                <div className="flex absolute right-10">
+                                                    <div className="flex flex-col border rounded-[6px] border-2 px-4 py-3 bg-white ">
+                                                        <div className="flex  text-[12px]">
+                                                            <img
+                                                                className="mr-2"
+                                                                src="\images\PencilSimpleLine.svg"
+                                                                alt="pencil"
+                                                            />
+                                                            Edit Details
+                                                        </div>
+                                                        <div className="flex items-center text-[12px] my-2">
+                                                            <img
+                                                                className="mr-2"
+                                                                src="\images\Phone.svg"
+                                                                alt="phone"
+                                                            />{" "}
+                                                            Contact Sellers
+                                                        </div>
+                                                        <div className="flex items-center text-[12px] text-[#DE0D0D]">
+                                                            <img
+                                                                className="mr-2"
+                                                                src="\images\Trash (2).svg"
+                                                                alt="Trash"
+                                                            />{" "}
+                                                            Delete Entry
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )}</td>
 
                                         {/* <td class="px-6 py-2 text-gray-800">{el.contact}</td> */}
                                     </tr>
@@ -324,6 +392,47 @@ const Inventory = () => {
                     </button>
                 </div>
             </div>
+
+            {modal && <>
+                <div className="flex w-[100vw] h-[100vh] d-flex justify-center items-center backdrop-blur-sm" style={{ top: "0", left: "0", position: "fixed" }}>
+                    <div className=" rounded-[6px] border-2 px-6 py-6 bg-white">
+                        <div className=" flex justify-end cursor-pointer" onClick={handleModalChange}>
+                            <img className="" src="\images\X.svg" alt="X" />
+                        </div>
+                        <div className="text-[20px] py-2">Update Inventory</div>
+
+                        <div className="text-[18px] pt-4 pb-2">Upload a bill or memo of your order</div>
+
+                        <div className="text-[18px] py-2">
+                            Scan bill/memo to automatically update your inventory
+                        </div>
+
+                        <div
+                            className="w-[45vw] flex flex-col py-14 justify-center items-center my-4 "
+                            onDrop={handleDrop}
+                            onDragOver={handleDragOver}
+                            style={{
+                                border: "1px dashed black",
+                            }}
+                        >
+                            <div>Drag and Drop Files Here</div>
+                            <input type="file" onChange={handleFileUpload} multiple />
+                            <div className="flex justify-center items-center">
+                                {uploadedFiles.map((file, index) => (
+                                    <div key={index}>{file.name}</div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center text-[18px] ">
+                            or
+                        </div>
+                        <div className="rounded-[6px] text-white text-[14px] bg-[#303F9F] border-2 border-[#7F56D9] py-4 my-2">
+                            <div className="flex justify-center items-center">Make a manual entry</div>
+                        </div>
+
+                    </div>
+                </div>
+            </>}
         </div>
     )
 }
