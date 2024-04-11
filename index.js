28db8c7a3cb633ccc4d204e25a3b890cfaa6a8b9
jainl28patel <jainilpatel2003@gmail.com>
2024-04-11 18:06:45 +0530

    add express

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 import "./loadEnv.mjs"
+import express from "express";
+import cors from "cors";
+import "./loadEnv.mjs";
+import "express-async-errors";
+import admin from "./src/routes/admin.mjs";
+import manager from "./src/routes/manager.mjs";
+import user from "./src/routes/user.mjs";
 
-import { usersModel, medicinesModel, ordersModel, salesModel, storesModel } from "./src/db/models.mjs";
+const PORT = process.env.PORT || 4000;
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+// Routes
+app.use("/admin", admin);
+app.use("/manager", manager);
+app.use("/user", user);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
