fabb6a44a1b433158295bfdb056e153935def70c
Shaurya Priyadarshi <shpr24680@gmail.com>
2024-04-11 15:58:02 +0530

    details and update inventory

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Mainpage = () => {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = [...e.dataTransfer.files];
+    setUploadedFiles(files);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFileUpload = (e) => {
+    const files = [...e.target.files];
+    setUploadedFiles(files);
+  };
+
   return (
     <div className=" py-6 px-6 bg-[#E2E8F0] w-[85vw]">
       <div className=" my-2">
@@ -114,14 +131,74 @@ const Mainpage = () => {
           </div>
         </div>
         <div className="flex items-center my-4 border-b-2 pb-8">
-          <div className="border-b-2 border-[#303F9F] text-[#303F9F] text-[16px] py-2 px-4">Symptoms</div>
+          <div className="border-b-2 border-[#303F9F] text-[#303F9F] text-[16px] py-2 px-4">
+            Symptoms
+          </div>
           <div className=" text-[16px] py-2 px-4">Medicine</div>
         </div>
-        <div className="py-4" >
+        <div className="py-4">
           <img src="\images\Frame 1244832715.svg" alt="graph" />
         </div>
       </div>
-      
+      <div className="flex">
+        <div className="flex flex-col border rounded-[6px] border-2  px-4 py-3 bg-white ">
+          <div className="flex  text-[12px]">
+            <img
+              className="mr-2"
+              src="\images\PencilSimpleLine.svg"
+              alt="pencil"
+            />
+            Edit Details
+          </div>
+          <div className="flex items-center text-[12px] my-2">
+            <img className="mr-2" src="\images\Phone.svg" alt="phone" /> Contact
+            Sellers
+          </div>
+          <div className="flex items-center text-[12px] text-[#DE0D0D]">
+            <img className="mr-2" src="\images\Trash (2).svg" alt="Trash" />{" "}
+            Delete Entry
+          </div>
+        </div>
+      </div>
+
+      <div className="flex">
+        <div className=" rounded-[6px] border-2 px-6 py-6 bg-white">
+          <div className=" flex justify-end ">
+            <img className="" src="\images\X.svg" alt="X" />
+          </div>
+          <div className="text-[20px] py-2">Update Inventory</div>
+
+          <div className="text-[18px] pt-4 pb-2">Upload a bill or memo of your order</div>
+
+          <div className="text-[18px] py-2">
+            Scan bill/memo to automatically update your inventory
+          </div>
+
+          <div
+            className="w-[45vw] flex flex-col py-14 justify-center items-center my-4 "
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            style={{
+              border: "1px dashed black",
+            }}
+          >
+            <div>Drag and Drop Files Here</div>
+            <input type="file" onChange={handleFileUpload} multiple />
+            <div className="flex justify-center items-center">
+              {uploadedFiles.map((file, index) => (
+                <div key={index}>{file.name}</div>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-center text-[18px] ">
+            or
+          </div>
+          <div className="rounded-[6px] text-white text-[14px] bg-[#303F9F] border-2 border-[#7F56D9] py-4 my-2">
+            <div className="flex justify-center items-center">Make a manual entry</div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
