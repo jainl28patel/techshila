dd9b973948c06fd1a3c207a804361ebdea7a0276
Shaurya Priyadarshi <shpr24680@gmail.com>
2024-04-11 20:42:21 +0530

    updated userpage with right page

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,8 +2,110 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../user_nav/Nav";
 import axios from "axios";
+import demomed from "../../assets/image2.png"
+import Select from "react-select";
+import "../Stores/Stores.scss";
 
 function User() {
+
+
+const [showSecondPage, setShowSecondPage] = useState(false);
+
+
+const RightPart = () => {
+    const options = [
+        { value: "Gopal Pharma and Labs", label: "Gopal Pharma and Labs" },
+        { value: "Laal Pathology", label: "Laal Pathology" },
+        { value: "Jain Pharma", label: "Jain Pharma" },
+    ];
+    return (
+        <div className="bg-cyan-50 h-[full] w-[98vw]">
+            <div className=' w-[38vw] height-[100vh] right-[0] bg-white' style={{ position: "absolute", right: "0", top: "8vh" }}>
+                <div className='w-[100%] px-5 py-5 cursor-pointer' onClick={() => setShowSecondPage(false)}>
+                    <img src="/images/X.svg" alt="close_button" />
+                </div>
+
+                <div className='flex flex-row gap-x-5 justify-center'>
+                    <div className='flex flex-col gap-y-2'>
+                        <div className='border-2 rounded-xl border-slate-500 w-[15vw] h-[30vh] flex justify-center items-center'>
+                            <img className='rounded-xl w-[8.5vw]' src={demomed} alt="demomed" />
+                        </div>
+                        <div className='flex gap-x-3.5'>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'> <img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col gap-y-1'>
+                        <div>Ecosprin 75mg Strip Of 14 Tablets</div>
+                        <div>
+                            BY MICRO LEVELS
+                        </div>
+                        <div>
+                            15 Tablet(s) in Strip
+                        </div>
+                        <div>
+                            MRP: <span style={{ textDecoration: "line-through" }}>&#8377;33.6</span>
+                        </div>
+                        <div>
+                            &#8377;28.90 <span className='text-red-600'>14% OFF</span>
+                        </div>
+                        <div>
+                            Inclusive of all taxes
+                        </div>
+                        <div>
+                            Quantity : <input type="number" className="border-2 rounded border-slate-500"></input>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col px-5 pt-5'>
+                    <div className='font-bold px-5'>Dolo 650 MG Description</div>
+                    <div className='px-5 pt-1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                </div>
+
+                <div className='flex flex-col px-5 pt-5'>
+                    <div className='px-5 font-bold'>
+                    Select Seller Store
+                    </div>
+                    <div className='px-5'>
+                        nearest store selected by default
+                    </div>
+                    <div className='px-5'>
+                    <Select
+          className="w-40"
+          classNamePrefix="select py-0 px-0"
+          defaultValue={options[0]}
+          isSearchable={true}
+          name="color"
+          options={options}
+        />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+  const toggleSecondPage = () => {
+    setShowSecondPage(true);
+    // console.log("sho")
+  };
+
+
+
+
   const orderedProducts = [
     {
       id: 1,
@@ -59,8 +161,8 @@ function User() {
     <div>
       <Nav />
       <div
-        style={{ width: "50vw", marginLeft: "25vw" }}
-        className="flex flex-col"
+        
+        className={showSecondPage ?"flex flex-col w-[40vw] ml-[10vw]":"flex flex-col  w-[50vw] ml-[25vw] "}
       >
         <div className=" font-bold flex justify-between mt-5">
           <span>What are you looking for?</span>
@@ -80,7 +182,7 @@ function User() {
               <div className="text-[24px] px-2">Previously bought</div>
               {/* <img src="\images\image 8.svg" alt="" /> */}
 
-              <div className="flex  text-[10px] py-2">
+              <div className="flex  text-[10px] py-2" onClick={toggleSecondPage}>
                 {/* Render the sample ordered products */}
                 {orderedProducts.map((product) => (
                   <div key={product.id} className="w-[8vw] mx-2">
@@ -190,7 +292,7 @@ function User() {
             </div>
           </div>
         ) : (
-          <div className="flex  text-[10px] py-2">
+          <div className="flex  text-[10px] py-2 " onClick={toggleSecondPage}>
                 {/* Render the sample ordered products */}
                 {filteredNodes.map((product) => (
                   <div key={product.id} className="w-[8vw] mx-2">
@@ -226,6 +328,7 @@ function User() {
         )}
 
       </div>
+      {showSecondPage&&<RightPart/>}
     </div>
   );
 }
