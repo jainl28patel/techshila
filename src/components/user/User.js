3233d72381812f4c7306df8bfb8eb2858d8b158e
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-04-12 10:09:35 +0530

    integrate user

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -5,19 +5,43 @@ import axios from "axios";
 import demomed from "../../assets/image2.png"
 import Select from "react-select";
 import "../Stores/Stores.scss";
+import { useGeolocated } from "react-geolocated";
 
 function User() {
 
 
-const [showSecondPage, setShowSecondPage] = useState(false);
-
+const [showSecondPage, setShowSecondPage] = useState(false); 
+const [item, setCurrItem] = useState({}); 
+const [stores, setStores] = useState([]); 
 
 const RightPart = () => {
+
+
     const options = [
         { value: "Gopal Pharma and Labs", label: "Gopal Pharma and Labs" },
         { value: "Laal Pathology", label: "Laal Pathology" },
         { value: "Jain Pharma", label: "Jain Pharma" },
     ];
+
+    const [quantity, setquantity] = useState('');
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const changequantity = (e) => {
+      setquantity(e.target.value);
+    }
+
+    const handelOrder = (med_id, quantity)=>{
+      axios.post("/user/place-order", {
+        med_id : med_id,
+        quantity:quantity,
+        store_id: selectedOption.id
+      })
+    }
+    const handleChange = (option) => {
+      setSelectedOption(option);  // Update the state to the selected option
+      console.log('Option selected:', option);
+    };
+
     return (
         <div className="bg-cyan-50 h-[full] w-[98vw]">
             <div className=' w-[38vw] height-[100vh] right-[0] bg-white' style={{ position: "absolute", right: "0", top: "8vh" }}>
@@ -28,40 +52,53 @@ const RightPart = () => {
                 <div className='flex flex-row gap-x-5 justify-center'>
                     <div className='flex flex-col gap-y-2'>
                         <div className='border-2 rounded-xl border-slate-500 w-[15vw] h-[30vh] flex justify-center items-center'>
-                            <img className='rounded-xl w-[8.5vw]' src={demomed} alt="demomed" />
+                            <img className='rounded-xl w-[8.5vw]' src={item.image_url} alt="demomed" />
                         </div>
                         <div className='flex gap-x-3.5'>
-                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'> <img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
-                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
-                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={demomed} alt="demomed" /></div>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'> <img className='rounded w-[2vw]' src={item.image_url} alt="demomed" /></div>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={item.image_url} alt="demomed" /></div>
+                            <div className='border-2 rounded-xl border-slate-500 w-[4.5vw] h-[10vh] flex justify-center items-center'><img className='rounded w-[2vw]' src={item.image_url} alt="demomed" /></div>
                         </div>
                     </div>
 
-                    <div className='flex flex-col gap-y-1'>
-                        <div>Ecosprin 75mg Strip Of 14 Tablets</div>
-                        <div>
-                            BY MICRO LEVELS
-                        </div>
-                        <div>
+                    <div className='flex flex-col gap-y-1 w-[15vw]'>
+                        <div className="font-bold text-[24px]">{item.name}</div>
+                        <p className="whitespace-normal break-words" >
+                            {item.desc}
+                        </p>
+                        {/* <div>
                             15 Tablet(s) in Strip
+                        </div> */}
+                        <div className="font-bold">
+                            Price: <span >&#8377;{item.price}</span>
                         </div>
-                        <div>
-                            MRP: <span style={{ textDecoration: "line-through" }}>&#8377;33.6</span>
-                        </div>
-                        <div>
+                        {/* <div>
                             &#8377;28.90 <span className='text-red-600'>14% OFF</span>
-                        </div>
+                        </div> */}
                         <div>
                             Inclusive of all taxes
                         </div>
                         <div>
-                            Quantity : <input type="number" className="border-2 rounded border-slate-500"></input>
+                            Quantity : <input type="number" className="border-2 rounded border-slate-500" value={quantity} onChange={changequantity}></input>
                         </div>
+                        <button
+                          type="submit"
+                          onClick={()=>handelOrder(item._id, quantity)}
+                          className="group relative w-3/4 flex justify-center mt-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                          Order
+                        </button>
                     </div>
                 </div>
-                <div className='flex flex-col px-5 pt-5'>
-                    <div className='font-bold px-5'>Dolo 650 MG Description</div>
-                    <div className='px-5 pt-1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                <div className='flex flex-col px-5 pt-5 mx-5'>
+                  <div >
+                    Quantity available:  {item.qty_available}
+                  </div>
+                  <div>
+                    Expiry Date : {item.expiry_date.substring(0 , item.expiry_date.indexOf('T'))}
+                  </div>
+                    {/* <div className='font-bold px-5'>Dolo 650 MG Description</div>
+                    <div className='px-5 pt-1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div> */}
                 </div>
 
                 <div className='flex flex-col px-5 pt-5'>
@@ -69,16 +106,18 @@ const RightPart = () => {
                     Select Seller Store
                     </div>
                     <div className='px-5'>
-                        nearest store selected by default
+                        Nearest store is at top by default
                     </div>
                     <div className='px-5'>
                     <Select
           className="w-40"
           classNamePrefix="select py-0 px-0"
-          defaultValue={options[0]}
+          defaultValue={stores[0]}
           isSearchable={true}
           name="color"
-          options={options}
+          options={stores}
+          onChange={handleChange}  // Set the onChange handler
+          value={selectedOption}   // Set the selected option
         />
                     </div>
                 </div>
@@ -98,66 +137,140 @@ const RightPart = () => {
 
 
 
-  const toggleSecondPage = () => {
+  const toggleSecondPage = (pro) => {
     setShowSecondPage(true);
+    setCurrItem(pro)
     // console.log("sho")
   };
 
 
 
 
-  const orderedProducts = [
-    {
-      id: 1,
-      name: "Sugar Free Gold",
-      imageUrl: "/images/Screenshot 2024-04-11 181121.png",
-      description:
-        "Sweetneer Poweder dslfjsldjfljsosdfhkjshdfkhdksdsfhsdjfhkhiwehi lorem djfsoifefdljfoisefjjofhki",
-      mrp: 100,
+  // const orderedProducts = [
+  //   {
+  //     id: 1,
+  //     name: "Sugar Free Gold",
+  //     imageUrl: "/images/Screenshot 2024-04-11 181121.png",
+  //     description:
+  //       "Sweetneer Poweder dslfjsldjfljsosdfhkjshdfkhdksdsfhsdjfhkhiwehi lorem djfsoifefdljfoisefjjofhki",
+  //     mrp: 100,
     
-    },
-    {
-      id: 2,
-      name: "Sud Good Medicine",
-      imageUrl: "/images/Screenshot 2024-04-11 181140.png",
-      description: "Gives you all the energy you want",
-      mrp: 120,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Sud Good Medicine",
+  //     imageUrl: "/images/Screenshot 2024-04-11 181140.png",
+  //     description: "Gives you all the energy you want",
+  //     mrp: 120,
 
-    },
-    // Add more sample products as needed
-  ];
+  //   },
+  //   // Add more sample products as needed
+  // ];
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(8);
 
-  // Filter data based on search term
-  const filteredNodes = orderedProducts.filter((orderedProduct) =>
-    orderedProduct.name.toLowerCase().includes(searchTerm.toLowerCase())
+
+ 
+  const [orderedProducts, setOrderedProducts] = useState([]);
+  const [loc, setLoc] = useState("Searching Location...");
+  const [userDetails, setUserDetails] = useState("");
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+        useGeolocated({
+            positionOptions: {
+                enableHighAccuracy: false,
+            },
+            userDecisionTimeout: 1000,
+        });
+
+  const updateLoc = (lat,long) =>{
+    axios.post("/user/update-location", {
+      latitude:lat,
+      longitude:long
+    })
+  }
+
+  const updateStores = () =>{
+    axios.get("/user/store-details")
+    .then((res) => {
+      if(res){
+        let store_list = []
+        res.data.map((item)=>[
+          store_list.push({
+            value: item.name,
+            label: item.name,
+            id: item._id
+          })
+        ])
+      setStores(store_list)
+      }
+
+
+    })
+  }
+
+  const setupdetails = () =>{
+    axios.get('/user/user-details' , {
+    })
+    .then(response => {
+      // console.log("response data",response.data)
+      console.log("details ",response)
+      setUserDetails(response.data)
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${userDetails.latitude},${userDetails.longitude}&key=AIzaSyBB5VtbYNkoqHZD9uPohGNszVQnSsCo5ko`)
+      .then((response) =>{
+        console.log("res", response)
+        let str = response.data.plus_code.compound_code
+        setLoc(str.substring(str.indexOf(' ') + 1))
+      }).catch((err)=>{
+        console.log(err)
+      })
+    })
+    .catch(error => {
+      console.error('Error fetching ordered products:', error);
+    });
+  }
+
+  useEffect(() => {
+    axios.get("/user/order-history")
+    .then(response => {
+      console.log("response data: ",response.data)
+      setOrderedProducts(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching ordered products:', error);
+      // console.log("hekjhekj")
+    });
+  }, []); 
+  
+  const [nearProducts, setnearProducts] = useState([]);
+  const filteredNodes = nearProducts.filter((nearProduct) =>
+    nearProduct.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // const [orderedProducts, setOrderedProducts] = useState([]);
+  useEffect(() => {
+    axios.get("/user/search" , {
+    })
+    .then(response => {
+      // console.log("response data",response.data)
+      setnearProducts(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching ordered products:', error);
+    });
+  }, []); 
 
-  // useEffect(() => {
-  //   // Fetch previously ordered products from the backend API
-  //   axios.get('https://your-backend-server.com/api/ordered-products', {
-  //     // Add authentication token if required by your backend
-  //     // headers: {
-  //     //   Authorization: `Bearer ${token}`
-  //     // }
-  //   })
-  //   .then(response => {
-  //     // Set the fetched products in state
-  //     setOrderedProducts(response.data);
-  //   })
-  //   .catch(error => {
-  //     console.error('Error fetching ordered products:', error);
-  //   });
-  // }, []); // Empty dependency array to ensure the effect runs only once
+  useEffect(() => {
+    setupdetails()
+    updateStores()
+    if(coords){
+      updateLoc(coords.latitude, coords.longitude)
+    }
+  }, [coords]); 
+
+
 
   return (
     <div>
-      <Nav />
+      <Nav props={ {name: userDetails.name, email: userDetails.email, loc: loc}} />
       <div
         
         className={showSecondPage ?"flex flex-col w-[40vw] ml-[10vw]":"flex flex-col  w-[50vw] ml-[25vw] "}
@@ -179,14 +292,13 @@ const RightPart = () => {
             <div>
               <div className="text-[24px] px-2">Previously bought</div>
               {/* <img src="\images\image 8.svg" alt="" /> */}
-
-              <div className="flex  text-[10px] py-2" onClick={toggleSecondPage}>
+              { orderedProducts === "no-order-history" ? <h1 style={{padding:"1rem 0 0 1rem", marginBottom: "3rem"}}>No order history</h1>:  <div className="flex  text-[10px] py-2" style={{marginBottom: "3rem"}} >
                 {/* Render the sample ordered products */}
                 {orderedProducts.map((product) => (
-                  <div key={product.id} className="w-[8vw] mx-2">
+                  <div key={product._id} className="w-[8vw] mx-2" onClick={() => toggleSecondPage(product)}>
                     <img
                       className="w-[8vw]"
-                      src={product.imageUrl}
+                      src={product.image_url}
                       alt={product.name}
                     />
                     <h3 className="font-bold">{product.name}</h3>
@@ -198,27 +310,27 @@ const RightPart = () => {
                         whiteSpace: "nowrap",
                       }}
                     >
-                      {product.description}
+                      {product.desc}
                     </p>
                     <p className="text-[8px]">
-                      MRP:{" "}
-                      <span className="line-through">Rs. {product.mrp}</span>
+                       Price:{" "}
+                      <span className="">Rs. {product.price}</span>
                     </p>
                      
                   </div>
                 ))}
-              </div>
+              </div>}
             </div>
             <div className="py-2">
-              <div className="text-[24px] px-2">Trending near you</div>
+              <div className="text-[24px] px-2">Available at your Location</div>
               {/* <img src="\images\image 6.svg" alt="" /> */}
               <div className="flex  text-[10px] py-2">
                 {/* Render the sample ordered products */}
-                {orderedProducts.map((product) => (
-                  <div key={product.id} className="w-[8vw] mx-2">
+                {nearProducts.map((product) => (
+                  <div key={product._id} className="w-[8vw] mx-2" onClick={() => toggleSecondPage(product)}>
                     <img
                       className="w-[8vw]"
-                      src={product.imageUrl}
+                      src={product.image_url}
                       alt={product.name}
                     />
                     <h3 className="font-bold">{product.name}</h3>
@@ -230,58 +342,27 @@ const RightPart = () => {
                         whiteSpace: "nowrap",
                       }}
                     >
-                      {product.description}
+                      {product.desc}
                     </p>
                     <p className="text-[8px]">
-                      MRP:{" "}
-                      <span className="line-through">Rs. {product.mrp}</span>
+                       Price:{" "}
+                      <span className="">Rs. {product.price}</span>
                     </p>
                      
                   </div>
                 ))}
               </div>
             </div>
-            <div>
-              <div className="text-[24px] px-2">Essentials</div>
-              {/* <img src="\images\image 7.svg" alt="" /> */}
-              <div className="flex  text-[10px] py-2">
-                {/* Render the sample ordered products */}
-                {orderedProducts.map((product) => (
-                  <div key={product.id} className="w-[8vw] mx-2">
-                    <img
-                      className="w-[8vw]"
-                      src={product.imageUrl}
-                      alt={product.name}
-                    />
-                    <h3 className="font-bold">{product.name}</h3>
-                    <p
-                      style={{
-                        width: "100px",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {product.description}
-                    </p>
-                    <p className="text-[8px]">
-                      MRP:{" "}
-                      <span className="line-through">Rs. {product.mrp}</span>
-                    </p>
-                     
-                  </div>
-                ))}
-              </div>
-            </div>
+            
           </div>
         ) : (
-          <div className="flex  text-[10px] py-2 " onClick={toggleSecondPage}>
+          <div className="flex  text-[10px] py-2 " >
                 {/* Render the sample ordered products */}
                 {filteredNodes.map((product) => (
-                  <div key={product.id} className="w-[8vw] mx-2">
+                  <div key={product._id} className="w-[8vw] mx-2" onClick={()=>toggleSecondPage(product)}>
                     <img
                       className="w-[8vw]"
-                      src={product.imageUrl}
+                      src={product.image_url}
                       alt={product.name}
                     />
                     <h3 className="font-bold">{product.name}</h3>
@@ -293,11 +374,11 @@ const RightPart = () => {
                         whiteSpace: "nowrap",
                       }}
                     >
-                      {product.description}
+                      {product.desc}
                     </p>
                     <p className="text-[8px]">
-                      MRP:{" "}
-                      <span className="line-through">Rs. {product.mrp}</span>
+                       Price:{" "}
+                      <span className="">Rs. {product.price}</span>
                     </p>
                      
                   </div>
@@ -312,4 +393,3 @@ const RightPart = () => {
 }
 
 export default User;
-
--- src/components/user_nav/Nav.js
+++ src/components/user_nav/Nav.js
@@ -1,6 +1,8 @@
+import axios  from "axios";
 import React, { useState } from "react";
+import { useEffect } from "react";
 
-function Nav() {
+function Nav( {props} ) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -24,7 +26,7 @@ function Nav() {
         <span className="font-semibold text-[18px] tracking-tight ml-2">
           MIM web
         </span>
-        <div className="mx-8 text-sm">400002, Mumbai</div>
+        <div className="mx-8 text-sm">{props.loc}</div>
       </div>
       <div className="flex items-center">
         <div className="mx-8 text-sm">Order Status</div>
@@ -38,7 +40,8 @@ function Nav() {
       {isModalOpen && (
         <div className="absolute top-10 right-5  flex  justify-end items-start">
           <div className="bg-slate-200 p-4 rounded-md flex flex-col items-center justify-center" >
-            <p> John Doe</p>
+            <p> {props.name} </p>
+            <p className="text-sm"> {props.email} </p>
             <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md" onClick={handlelogout}>
               Logout
             </button>
